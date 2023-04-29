c7c7a08a2fdfbd1fcbd01c3eea03add931e61b21
ycheryl <yulcheryl2357@gmail.com>
2023-04-29 18:51:56 +0700

    add combinations calculator

---
+++ Combinations.cpp
@@ -0,0 +1,57 @@
+/*
+ * Files: Combinations.cpp
+ * ------------------------------------------------------
+ * This program computes the mathematical function C(n,k)
+ * from its mathematical definition in terms of factorials
+ */
+
+#include <iostream>
+using namespace std;
+
+/* Function prototype */
+int combinations(int n, int k);
+int fact(int n);
+
+/* Main program */
+int main()
+{
+    int n, k;
+    cout << "Enter the number of objects: ";
+    cin >> n;
+    cout << "Enter the number to be chosen: ";
+    cin >> k;
+    cout << "C(n, k) = " << combinations(n, k) << endl;
+    return 0;
+}
+
+/*
+ * Function: Combinations(n,k)
+ * Usage: int nWays = combinations(n,k)
+ * ------------------------------------------------------
+ * Returns the mathematical combinations function C(n,k),
+ * which is the number of ways one can choose k elements
+ * from a set of size n
+ */
+
+int combinations(int n, int k)
+{
+    return fact(n) / (fact(k) * fact(n - k));
+}
+
+/*
+ * Function: fact(n)
+ * Usage: int result = fact(n)
+ * ------------------------------------------------------
+ * Returns the factorial of n, which is the product of all
+ * the integers between 1 and n, inclusive
+ */
+
+int fact(int n)
+{
+    int result = 1;
+    for (int i = 1; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}

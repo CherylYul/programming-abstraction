bd6c4a677c56d12a5155649aec6d6e5e52530cce
ycheryl <yulcheryl2357@gmail.com>
2023-06-03 23:50:35 +0700

    create error library

---
+++ error/error.h
@@ -0,0 +1,14 @@
+/*
+ * File: error.h
+ * ---------------------------------------------
+ * This interface exports handle error problem
+ */
+
+#pragma once
+#ifndef _error_h
+#define _error_h
+#include <string>
+
+void error(std::string msg);
+
+#endif
